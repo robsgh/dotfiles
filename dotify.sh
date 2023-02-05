0c46e50079f756d9482ab30fc6acf39c2ed16383
Rob Schmidt <rwschmidt26@gmail.com>
2023-02-04 21:16:41 -0800

    [symlink] Changed `cp -r` to `cp -rsf` to symlink dots
    Symlinking dots means that I don't have to keep running the ./dotify.sh
    script anymore, which is pretty neat!

--- dotify.sh
+++ dotify.sh
@@ -1,22 +1,51 @@
 #!/usr/bin/env bash
 
-cp ./.zshrc $HOME/.zshrc
-echo "Copied .zshrc"
+PACKAGES=(
+    i3
+    neovim
+    polybar
+    alacritty
+    picom
+    rofi
+    xset
+    xinput
+    xss-lock
+    feh
+    i3lock
+)
+
+if [[ ! -e "$HOME/.oh-my-zsh" ]]; then
+    echo "Installing oh-my-zsh..."
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
+
+echo "Copying .zshrc..."
+cp -sf "$HOME/dots/.zshrc" "$HOME/.zshrc"
 
 for dirname in `ls .config`; do
-    if [ ! -d "$HOME/.config/$dirname" ]; then
+    if [[ ! -e "$HOME/.config/$dirname" ]] || [[ ! -d "$HOME/.config/$dirname" ]]; then
         echo "Creating config dir \"$HOME/.config/$dirname\"..."
         mkdir -p "$HOME/.config/$dirname"
     fi
+
     echo "Copying \".config/$dirname/*\" to \"$HOME/.config/$dirname/\""
-    cp -r .config/$dirname/* "$HOME/.config/$dirname/"
+    cp -rsf "$HOME/dots/.config/$dirname/." "$HOME/.config/$dirname/"
 done
 
 echo "Bootstrapping packer in neovim..."
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync' &2>/dev/null
-
-if [ 0 -eq $? ]; then
+if nvim --headless -c "autocmd User PackerComplete quitall" -c "PackerSync" 2&> /dev/null; then
     echo "Packer bootstrapped successfully."
 else
     echo "An error ocurred during Packer bootstrap."
 fi
+
+echo "Installing required packages..."
+for package in "${PACKAGES[@]}"; do
+    if ! dnf list --installed "$package" > /dev/null; then
+        echo "Installing $package..."
+        sudo dnf install -y $package
+    else
+        echo "$package is installed!"
+    fi
+done
+
