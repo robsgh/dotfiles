98281db98c396ebf5b548a4210bbd9209a2204fa
Rob Schmidt <rob+git@schminfra.net>
2024-04-07 12:58:20 -0700

    [dunst] added dunst

--- dotify.sh
+++ dotify.sh
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
 PACKAGES=(
-	neovim
-	hyprland-git
-	hyprpaper
-	waybar-git
-	hyprlock
+	dunst
+	emoji-picker
+	fuzzel
+	grim
+	grimblast
+	hyprcursor
 	hypridle
+	hyprland
+	hyprlang
+	hyprlock
+	hyprpaper
+	hyprpicker
+	hyprshot
+	imv
+	kitty
+	neovim
+	waybar-git
+	xdg-desktop-portal-hyprland
 )
 
 DOTS_DIR="$(realpath -s $(dirname -- "$0"))"
@@ -23,14 +35,7 @@ echo "Copying .tmux.conf..."
 cp -sf "$DOTS_DIR/.tmux.conf" "$HOME/.tmux.conf"
 
 echo "Installing required packages..."
-for package in "${PACKAGES[@]}"; do
-	if ! dnf list --installed "$package" >/dev/null; then
-		echo "Installing $package..."
-		sudo dnf install -y $package
-	else
-		echo "$package is installed!"
-	fi
-done
+sudo dnf install -y "${PACKAGES[@]}"
 
 if ! fc-list -q :family="SauceCodePro Nerd Font"; then
 	echo "Installing SauceCodePro Nerd Font..."
@@ -42,14 +47,14 @@ else
 	echo "SauceCodePro Nerd Font is already installed... skipping download"
 fi
 
-for dirname in $(ls .config); do
+for dirname in $(ls "$DOTS_DIR"/config); do
 	if [[ ! -e "$HOME/.config/$dirname" ]] || [[ ! -d "$HOME/.config/$dirname" ]]; then
 		echo "Creating config dir \"$HOME/.config/$dirname\"..."
 		mkdir -p "$HOME/.config/$dirname"
 	fi
 
-	echo "Copying \".config/$dirname/*\" to \"$HOME/.config/$dirname/\""
-	cp -rsf "$DOTS_DIR/.config/$dirname/." "$HOME/.config/$dirname/"
+	echo "Copying and linking \"config/$dirname/*\" to \"$HOME/.config/$dirname/\""
+	cp -rsf "$DOTS_DIR/config/$dirname/." "$HOME/.config/$dirname/"
 done
 
 if [[ ! -e "$HOME/.tmux/plugins/tpm" ]]; then
