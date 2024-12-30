89d78e5600c494f01ea6f3757cf6065d84a682f7
Robert Schmidt <rwschmidt26@gmail.com>
2024-12-30 00:17:06 -0600

    Updated setup.sh and added zsh config

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -3,3 +3,13 @@
 -- Add any additional options here
 
 vim.opt.background = "dark" -- or "light" for light mode
+vim.opt.number = true
+vim.cmd([[
+  augroup numbertoggle
+    autocmd!
+    autocmd BufEnter,FocusGained,InsertLeave,WinEnter * :set relativenumber
+    autocmd BufLeave,FocusLost,InsertEnter,WinLeave * :set norelativenumber
+  augroup END
+]])
+
+vim.opt.clipboard = ""
--- setup.sh
+++ setup.sh
@@ -1,4 +1,28 @@
-#!/usr/bin/env zsh
+#!/usr/bin/env bash
 
-ln -s ~/dotfiles/nvim ~/.config/nvim
-ln -s ~/dotfiles/ghostty ~/.config/ghostty
+if [[ -L ~/.config/nvim ]]; then
+  echo "nvim is already symlinked, skipping"
+else
+  echo "Linking nvim config dir"
+  ln -s ~/dotfiles/nvim ~/.config/nvim
+fi
+
+if [[ -L ~/.config/ghostty ]]; then
+  echo "ghostty is already symlinked, skipping"
+else
+  echo "Linking ghostty config dir"
+  ln -s ~/dotfiles/ghostty ~/.config/ghostty
+fi
+
+if [[ -L ~/.zshrc ]]; then
+  echo ".zshrc is already a symlink, skipping"
+else
+  if [[ -f ~/.zshrc ]]; then
+    echo "Moving zshrc to a backup"
+    mv ~/.zshrc ~/.zshrc.bak
+  fi
+
+  echo "Linking zshrc"
+  ln -s ~/dotfiles/zshrc ~/.zshrc
+  echo "    REMINDER: You should source the new .zshrc now"
+fi
