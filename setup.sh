6bb4d0e2db9eeb4e590f8cfb152b29b8ba3e90db
Rob Schmidt <rob+git@schminfra.net>
2025-04-12 12:24:49 -0700

    updating

--- setup.sh
+++ setup.sh
@@ -27,5 +27,4 @@ link_maybe_backup nvim ~/.config/nvim
 link_maybe_backup ghostty ~/.config/ghostty
 link_maybe_backup zshrc ~/.zshrc
 link_maybe_backup tmux.conf ~/.tmux.conf
-link_maybe_backup hypr ~/.config/hypr
-link_maybe_backup waybar ~/.config/waybar
+link_maybe_backup i3 ~/.config/i3
