2c1afbb9f3fbf937cda98be137b0af367f8e02f8
Rob Schmidt <rob@schminfra.net>
2025-11-12 18:23:31 -0800

    [cleanup] remove old configs that I don't actually use anymore

--- hypr/wallpaperize.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-WALLPAPERS="$HOME/Wallpapers/"
-CURRENT_WALLPAPER=$(hyprctl hyprpaper listloaded)
-
-WALLPAPER=$(find "$WALLPAPERS" -type f ! -name "$(basename "$CURRENT_WALLPAPER")" | shuf -n 1)
-
-hyprctl hyprpaper reload ,"$WALLPAPER"
