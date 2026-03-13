f3e26a061a65ab1460d628b3c5b9054a6789dcfb
Rob Schmidt <rob+git@schminfra.net>
2026-03-12 20:14:34 -0700

    unfck hyprland bindings
    Getting sick of needing to unwind omarchy bindings..

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ set -e
 function link_maybe_backup() {
   local config="$(realpath ./$1)"
   local filename="$2"
+  local filepath="$(dirname $filename)"
 
   if [[ -L "$filename" ]]; then
     echo "$filename is already symlinked, skipping"
@@ -15,11 +16,12 @@ function link_maybe_backup() {
     mv "$filename" "$filename.bak"
   fi
 
+  mkdir -p "$filepath"
   ln -sv "$config" "$filename"
 }
 
 link_maybe_backup nvim ~/.config/nvim
 link_maybe_backup ghostty ~/.config/ghostty
 link_maybe_backup bashrc ~/.bashrc
-link_maybe_backup tmux.conf ~/.tmux.conf
+link_maybe_backup tmux.conf ~/.config/tmux/tmux.conf
 link_maybe_backup hypr ~/.config/hypr
